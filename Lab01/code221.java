e5ef1ac288483dfece1f5c3ce96dc38523fd9c06
Trần Duy Khánh <117696770+KazuhaVN@users.noreply.github.com>
2023-04-17 09:40:00 +0700

    Create code221.java

---
+++ Lab01/code221.java
@@ -0,0 +1,8 @@
+package lesson1oop;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Xin chao \n Cac ban!");
+		System.out.println("Hello\t, World");
+	}
+}
